ad90786f20e51266488901fca6027db910781593
youngsavage-codes <117549855+youngsavage-codes@users.noreply.github.com>
2023-09-14 15:29:56 +0100

    update

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tailwind from 'twrnc'
 import FIcon from 'react-native-vector-icons/Feather'
@@ -7,10 +7,18 @@ import Icon from 'react-native-vector-icons/AntDesign'
 export default function BottomNav() {
   return (
     <View style={tailwind`bg-[#FA5758] absolute w-full flex-row items-center justify-around bottom-0 z-50 rounded-tl-20 rounded-tr-20 py-4`}>
-      <FIcon name='home' size={20} color='white'/>
-      <FIcon name='heart' size={20} color='gray'/>
-      <FIcon name='shopping-bag' size={20} color='gray'/>
-      <FIcon name='user' size={20} color='gray'/>
+               <TouchableOpacity>
+                               <FIcon name='home' size={20} color='white'/>
+               </TouchableOpacity>
+               <TouchableOpacity>
+                              <FIcon name='heart' size={20} color='black'/>
+               </TouchableOpacity>
+               <TouchableOpacity>
+                              <FIcon name='shopping-bag' size={20} color='black'/>
+               </TouchableOpacity>
+               <TouchableOpacity>
+                              <FIcon name='user' size={20} color='black'/>
+               </TouchableOpacity>
     </View>
   )
 }
