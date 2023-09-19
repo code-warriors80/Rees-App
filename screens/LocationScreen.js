580e1fc3e60dc89f2e381995b1279bc1c757e231
Abdulrahman Sadiq <61137642+master-keyCW@users.noreply.github.com>
2023-09-19 19:26:00 -0400

    git commit -m

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps';
 
 export default function LocationScreen() {
   return (
     <View>
-      <Text>LocationScreen</Text>
+      <Text>Location</Text>
+
+      <View>
+        <View style={{ flex: 1 }}>
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
+      </View>
     </View>
   )
 }
